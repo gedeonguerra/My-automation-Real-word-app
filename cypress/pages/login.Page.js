3861b10b224731c38595dbcb7fefc4f64faaf11c
Gedeon Guerra QA Engineer <netoguerra360@gmail.com>
2025-06-20 19:27:42 -0300

    login.Page.js

--- cypress/pages/login.Page.js
+++ cypress/pages/login.Page.js
@@ -1,38 +1,37 @@
-
+// Classe que representa a página de login da aplicação
 class LoginPage {
 
+    // Retorna os seletores usados na página de login
     selectorsList() {
         const selectors = {
-
-        usernameField: "[name='username']",
-        PasswordField: "[name='password']",
-        loginButton: "[type='submit']",
-        wrongCredentialAlert: "[role='alert']",
-        
-
+            // Campo para inserir o nome de usuário
+            usernameField: "[name='username']",
+            // Campo para inserir a senha (repare que 'PasswordField' está com P maiúsculo, manter consistência pode ser legal)
+            PasswordField: "[name='password']",
+            // Botão para enviar o formulário de login
+            loginButton: "[type='submit']",
+            // Alerta que aparece em caso de credenciais incorretas
+            wrongCredentialAlert: "[role='alert']",
         }
         return selectors 
     }
 
+    // Acessa a página de login via URL direta
     accessLoginPage() {
         cy.visit('http://localhost:3000/signin')
     }
 
-
+    // Realiza o login preenchendo os campos e clicando no botão
     loginWithUser(username, password) {
         cy.get(this.selectorsList().usernameField).type(username)
         cy.get(this.selectorsList().PasswordField).type(password)
         cy.get(this.selectorsList().loginButton).click()
     }
 
+    // Verifica se o alerta de credenciais inválidas está presente na tela
     checkAccessInvalid() {
         cy.get(this.selectorsList().wrongCredentialAlert)
     }
-
-    
-    
-    
 }
+
 export default LoginPage
-
-
